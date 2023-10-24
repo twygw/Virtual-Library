eac03c89a18262c806f65fb95207369a5fef6cfc
helal kohzad <helalkohzad@gmail.com>
2023-10-24 12:47:26 +0100

    Helal's Commit

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,21 @@
 // import the Media class:
-
+const media = require("./Media");
 // create your Book class:
+class Book extends Media {
+    constructor(author, numPages, rating) {
+        super(title, year, genre);
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
 
+    summary() {
+        return `Title ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count:: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    static highestRating(Book) {
+        return Math.max(Book.rating);
+    }
+}
 // don't change below
 module.exports = Book;
